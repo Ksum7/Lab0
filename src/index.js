1cee47d29198517cbe66fceb9af43a42b83abc4a
Ksum7 <xamchik02@gmail.com>
2024-09-11 13:27:52 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-alert("Let's integrate!")
+const N = 1000;
+
+function trapezoidalRule(func, a, b, n) {
+    const h = (b - a) / n;
+    let sum = (func(a) + func(b)) / 2;
+
+    for (let i = 1; i < n; i++) {
+        const x = a + i * h;
+        sum += func(x);
+    }
+
+    return sum * h;
+}
+
+function f4(x) {
+    return x * x - x + 1;
+}
+
+function calculateIntegration() {
+    const a = parseFloat(prompt("Введите нижний предел интегрирования (a):"));
+    const b = parseFloat(prompt("Введите верхний предел интегрирования (b):"));
+
+    if (isNaN(a) || isNaN(b)) {
+        alert("Пожалуйста, введите корректные значения.");
+        return;
+    }
+    const result = trapezoidalRule(f4, a, b, N);
+
+    document.getElementById("result").textContent = `Результат интегрирования: ${result}`;
+}
